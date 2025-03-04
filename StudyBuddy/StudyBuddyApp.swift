8acc953b9dcecdf10f68656972d7a7fe452458dc
Brendan Dasilva <brendan.dasilva@gmail.com>
2025-03-04 08:39:47 -0500

    Initial Commit

---
+++ StudyBuddy/StudyBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyBuddyApp.swift
+//  StudyBuddy
+//
+//  Created by Brendan Dasilva on 2025-03-04.
+//
+
+import SwiftUI
+
+@main
+struct StudyBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StudyBuddyTests/StudyBuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  StudyBuddyTests.swift
+//  StudyBuddyTests
+//
+//  Created by Brendan Dasilva on 2025-03-04.
+//
+
+import Testing
+@testable import StudyBuddy
+
+struct StudyBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
