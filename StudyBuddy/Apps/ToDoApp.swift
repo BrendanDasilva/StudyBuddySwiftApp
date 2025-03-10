8e6fa4c5f622169c06028b13fa2b6202f05c39ea
Brendan Dasilva <brendan.dasilva@gmail.com>
2025-03-10 09:26:28 -0400

    basic to-do list added - adapted from a react component I used for a different project - will make some adjustments for improved layout

--- StudyBuddy/Apps/ToDoApp.swift
+++ StudyBuddy/Apps/ToDoApp.swift
@@ -8,14 +8,94 @@
 import SwiftUI
 
 struct ToDoApp: View {
+    @State private var tasks: [String] = [] // store tasks
+    @State private var newTask: String = "" // stores input value
+    
     var body: some View {
         VStack {
-            Text("To-Do List App")
+            Text("To-Do List")
                 .font(.largeTitle)
                 .fontWeight(.bold)
-                .foregroundColor(.white)
+                .foregroundStyle(.white)
+                .padding()
+            
+            // input area
+            InputArea(newTask: $newTask, tasks: $tasks)
+                .padding(.horizontal, 20)
+            
+            // task list
+            List {
+            ForEach(tasks, id: \..self) { task in
+                ToDoItem(task: task, tasks: $tasks)
+            }
+            .onDelete { indexSet in
+                tasks.remove(atOffsets: indexSet)
+            }
         }
+        .listStyle(PlainListStyle())
+    }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(hex: "8AACEA").edgesIgnoringSafeArea(.all))
     }
+}
+
+
+// input area component
+struct InputArea: View {
+    @Binding var newTask: String
+    @Binding var tasks: [String]
+    
+    var body: some View {
+        HStack {
+            TextField("Enter a task", text: $newTask)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Button(action: {
+                if !newTask.isEmpty {
+                    tasks.append(newTask)
+                    newTask = ""
+                }
+            }) {
+                Image(systemName: "plus")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .clipShape(Circle())
+            }
+        }
+    }
+}
+
+
+
+// todo item component
+struct ToDoItem: View {
+    let task: String
+    @Binding var tasks: [String]
+    
+    var body: some View {
+        HStack {
+            Text(task)
+                .foregroundColor(.black)
+                .padding()
+            Spacer()
+            
+            Button(action: {
+                if let index = tasks.firstIndex(of: task) {
+                    tasks.remove(at: index)
+                }
+            }) {
+                Image(systemName: "trash")
+                    .foregroundColor(.red)
+            }
+        }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(10)
+    }
+}
+
+
+#Preview {
+    ToDoApp()
 }
