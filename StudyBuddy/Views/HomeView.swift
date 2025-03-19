6bb6e6ce1485b96ce1dce52a2c5c2daec8e015b3
Kailie Field <kailie.field@gmail.com>
2025-03-19 14:07:17 -0400

    adjusted custom fonts for HomeView, added shadows and adjusted for colour matching

--- StudyBuddy/Views/HomeView.swift
+++ StudyBuddy/Views/HomeView.swift
@@ -14,24 +14,27 @@ struct HomeView: View {
                 Color(hex: "8AACEA").edgesIgnoringSafeArea(.all)
                 VStack(spacing: 30) {
                     Text("STUDY\nBUDDY")
-                        .font(.system(size: 50, weight: .bold))
-                        .foregroundColor(.white)
+                        .font(.custom("Anybody SemiBold", size: 96))
+                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                        .shadow(color: Color(#colorLiteral(red: 0.3333333433, green: 0.3333333433, blue: 0.3333333433, alpha: 1)), radius: 8, x: 0, y: 8)
                         .multilineTextAlignment(.center)
 
                     VStack(spacing: 15) {
                         NavigationLink(destination: LoginView()) {
                             Text("Login")
-                                .frame(width: 200, height: 50)
+                                .frame(width: 300, height: 50)
                                 .background(Color.clear)
-                                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.purple, lineWidth: 2))
+                                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color(#colorLiteral(red: 0.3562943339, green: 0.2727283835, blue: 0.5644717216, alpha: 1)), lineWidth: 2))
+                                .shadow(color: Color(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)), radius: 4, x: 0, y: 4)
                                 .foregroundColor(.white)
                         }
 
                         NavigationLink(destination: RegisterView()) {
                             Text("Sign Up")
-                                .frame(width: 200, height: 50)
+                                .frame(width: 300, height: 50)
                                 .background(Color.clear)
-                                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.purple, lineWidth: 2))
+                                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color(#colorLiteral(red: 0.3562943339, green: 0.2727283835, blue: 0.5644717216, alpha: 1)), lineWidth: 2))
+                                .shadow(color: Color(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)), radius: 4, x: 0, y: 4)
                                 .foregroundColor(.white)
                         }
                     }
