91421527ad0a309c886257c5435528d2282a9665
Kailie Field <kailie.field@gmail.com>
2025-03-19 16:51:38 -0400

    adjusting UI component colours and shadows

--- StudyBuddy/Views/LoginView.swift
+++ StudyBuddy/Views/LoginView.swift
@@ -19,9 +19,8 @@ struct LoginView: View {
                 Text("STUDY\nBUDDY")
                     .padding(.top, 180)
                     .font(.custom("HelveticaNeue-Bold", size: 96))
-//                    .font(.custom("Anybody", size: 96)) //<--- need to figure out custom font misconfiguration
                     .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                    .shadow(color: Color(#colorLiteral(red: 0.3333333433, green: 0.3333333433, blue: 0.3333333433, alpha: 1)), radius: 8, x: 0, y: 8)
+                    .shadow(color: Color(#colorLiteral(red: 0.3333333433, green: 0.3333333433, blue: 0.3333333433, alpha: 1)), radius: 8, x: 0, y: -8)
                     .multilineTextAlignment(.center)
                 
                 VStack(alignment: .leading, spacing: 10) {
@@ -29,22 +28,20 @@ struct LoginView: View {
                         .font(.custom("Menlo", size: 16))
                     TextField("user@domain.com", text: $email)
                         .font(.custom("Menlo", size: 16))
-//                        .font(.custom("Inter Regular", size: 16))
-                        .foregroundColor(Color(#colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)))
+                        .foregroundColor(Color(#colorLiteral(red: 0.3499752283, green: 0.3499752283, blue: 0.3499751687, alpha: 1)))
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                     
                     Text("Password").foregroundColor(.white)
                         .font(.custom("Menlo", size: 16))
                     SecureField("******", text: $password)
                         .font(.custom("Menlo", size: 16))
-//                        .font(.custom("Inter Regular", size: 16))
-                        .foregroundColor(Color(#colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)))
+                        .foregroundColor(Color(#colorLiteral(red: 0.3482678346, green: 0.3482678346, blue: 0.3482678346, alpha: 1)))
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                     
                     Toggle("Remember Me", isOn: $rememberMe)
                         .font(.custom("Menlo", size: 16))
-//                        .font(.custom("Inter Regular", size: 15))
                         .foregroundColor(.white)
+                    
                 }
                 .padding(.horizontal, 30)
                 
@@ -53,7 +50,7 @@ struct LoginView: View {
                     .frame(width: 200, height: 50)
                     .background(Color(#colorLiteral(red: 0.534640789, green: 0.4167757928, blue: 0.865655005, alpha: 1)))
                     .cornerRadius(10)
-                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color(#colorLiteral(red: 0.3141242862, green: 0.2420060337, blue: 0.5135086775, alpha: 1)), lineWidth: 2))
+                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color(#colorLiteral(red: 0.4175539088, green: 0.3225862927, blue: 0.697378627, alpha: 0.6353218129)), lineWidth: 2))
                     .shadow(color: Color(#colorLiteral(red: 0.1370129638, green: 0.1044493588, blue: 0.2434654624, alpha: 0.5061568709)), radius: 2, x: 0, y: 2)
                     .foregroundColor(.white)
                 
